882d99eb08f313244b132430c5fbec546ee59996
el-hachemi <a.e.boukara@gmail.com>
2021-08-16 20:27:24 +0100

    Finishing checking for the argument to be an decimal degit and there's only one argument and that is at less one argument

--- week2/pset2/caesar/caesar.c
+++ week2/pset2/caesar/caesar.c
@@ -1,6 +1,38 @@
 #include<stdio.h>
 #include<cs50.h>
+#include<string.h>
 
-int main(int argc, char argv[])
+int main(int argc, string argv[])
 {
-   
+  // When there's no an argument
+  if (argc == 1)
+  {
+      printf("Error: Enter an argument after ./caesar\n");
+      return 1;
+  }
+  // When there's more than one argument
+  else if (argc > 2)
+  {
+      printf("Error: Only one argument is required\n");
+      return 1;
+  }
+  // When there's only one argument
+  else
+  {
+      string k = argv[1];
+      // Checking if any element of the string isn't a decimal number
+      for (int i = 0, n = strlen(k); i < n; i++)
+      {
+          if (k[i] < 48 || k[i] > 57) 
+          {
+              printf("Usage: ./caesar key\n");
+              return 1;
+          }
+      }
+      // Prompting the user for the plain text
+      string p = get_string("plaintext: ");
+      // Printing ciphertext without a new line to be able to continue after
+      printf("ciphertext: ");
+  }
+
+}
