7a3416b269a1daf9d24c8a451cb9c134fd1053ec
el-hachemi <a.e.boukara@gmail.com>
2021-08-20 02:34:06 +0100

    I'm at line 58

--- week2/pset2/substitution/substitution.c
+++ week2/pset2/substitution/substitution.c
@@ -28,14 +28,14 @@ int main(int argc, string argv[])
        {
            for (int i = 0, n = length; i < n; i++)         
            {
-               if (!isalpha(k(i))) // If any char isn't alphabetic
+               if (!isalpha(argv[1][i])) // If any char isn't alphabetic
                {
                    printf("Key must only contain alphabetic characters\n");
                    return 1;
                }
                for (int j = 0; j < n ; j++)                  
                {
-                  if (i != j && k[j] == k[i]) // If the same char is reapeat.
+                  if (i != j && argv[1][j] == argv[1][i]) // If the same char is reapeated.
                   {
                     printf("Key must not contain repeated characters.\n");
                     return 1;
@@ -45,37 +45,44 @@ int main(int argc, string argv[])
        }
    }
    
-string k = argv[1];
-      
-// Converting the decimal string to integer as it is
-int key = atoi(k);
-      
-// Prompting the user for the plain text
-string p = get_string("plaintext:  ");
-printf("ciphertext: ");
-// Initializing a cipher char (string is not necessary)
-      int c;      
-      for (int j = 0, N = strlen(p); j < N; j++)
-      {
-          if (p[j] > 64 && p[j] < 91)
-          {
-               p[j] = p[j] - 65;
-               c = (p[j] + key) % 26;
-               c = c + 65;
-          }
-          else if (p[j] > 96 && p[j] < 123)
-          {
-                p[j] = p[j] - 97;
-                c = (p[j] + key) % 26;
-                c = c + 97;
-          }
-          else
-          {
-              c = p[j];
-          }
-          printf("%c", c);
-      }
-      printf("\n");
-      return 0;
-  }
+    int k; 
+    int key[26];  // Validating the key as "key" variable
+    for (int i = 0; i < 26; i++)
+    {
+        argv[1][i] = toupper(argv[1][i]);
+        k = (argv[1][i] + 65) % 26;
+        key[i] = k;
+        printf("%i\n", key[i]);
+    }
+    
+    // Prompting the user for the plain text
+    string plain = get_string("plaintext:  ");
+    int p[] = plain;
+    printf("ciphertext: ");
+
+    // Initializing a cipher char (string is not necessary)
+    int c;      
+    for (int j = 0, n = strlen(plain); j < n; j++)
+    {
+        if (p[j] > 64 && p[j] < 91)
+        {
+            p[j] = p[j] - 65;
+            c = (p[j] + key) % 26;
+            c = c + 65;
+        }
+        else if (p[j] > 96 && p[j] < 123)
+        {
+            p[j] = p[j] - 97;
+            c = (p[j] + key) % 26;
+            c = c + 97;
+        }
+        else
+        {
+            c = p[j];
+        }
+        printf("%c", c);
+    }
+    printf("\n");
+    return 0;
 }
+
