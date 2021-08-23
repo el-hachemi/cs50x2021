d602aaacdcb01b7de7085122ece90a7caf612e9f
el-hachemi <a.e.boukara@gmail.com>
2021-08-23 01:05:19 +0100

    I'm now int problem set 3 with the file plurality.c in the folder plurality within the pset3

---
+++ week3/lab3/time.sh
@@ -0,0 +1,126 @@
+#!/usr/bin/env bash
+
+echo "WORST-CASES"
+echo "sort1-worst-case-5000"
+sudo /usr/bin/time -o /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort1 reversed5000.txt
+
+echo "sort1-worst-case-10000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort1 reversed10000.txt
+
+echo "sort1-worst-case-50000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort1 reversed50000.txt
+
+
+echo "RANDOM-CASES"
+echo "sort1-random-case-5000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort1 random5000.txt
+
+echo "sort1-random-case-10000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort1 random10000.txt
+
+echo "sort1-random-case-50000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort1 random50000.txt
+
+
+echo "BEST-CASES"
+echo "sort1-best-case-5000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort1 sorted5000.txt
+
+echo "sort1-best-case-10000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort1 sorted10000.txt
+
+echo "sort1-best-case-50000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort1 sorted50000.txt
+
+
+echo "WORST-CASES"
+echo "sort2-worst-case-5000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort2 reversed5000.txt
+
+echo "sort2-worst-case-10000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort2 reversed10000.txt
+
+echo "sort2-worst-case-50000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort2 reversed50000.txt
+
+
+echo "RANDOM-CASES"
+echo "sort2-random-case-5000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort2 random5000.txt
+
+echo "sort2-random-case-10000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort2 random10000.txt
+
+echo "sort2-random-case-50000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort2 random50000.txt
+
+
+echo "BEST-CASES"
+echo "sort2-best-case-5000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort2 sorted5000.txt
+
+echo "sort2-best-case-10000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort2 sorted10000.txt
+
+echo "sort2-best-case-50000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort2 sorted50000.txt
+
+
+echo "WORST-CASES"
+echo "sort3-worst-case-5000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort3 reversed5000.txt
+
+echo "sort3-worst-case-10000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort3 reversed10000.txt
+
+echo "sort3-worst-case-50000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort3 reversed50000.txt
+
+
+echo "RANDOM-CASES"
+echo "sort3-random-case-5000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort3 random5000.txt
+
+echo "sort3-random-case-10000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort3 random10000.txt
+
+echo "sort3-random-case-50000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort3 random50000.txt
+
+
+echo "BEST-CASES"
+echo "sort3-best-case-5000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort3 sorted5000.txt
+
+echo "sort3-best-case-10000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort3 sorted10000.txt
+
+echo "sort3-best-case-50000"
+sudo /usr/bin/time -ao /home/el-hachemi/time.txt
+/home/el-hachemi/cs50x2021/week3/lab3/sort3 sorted50000.txt
